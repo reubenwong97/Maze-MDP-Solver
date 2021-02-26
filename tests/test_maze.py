d30c7ef2a9120f2eb10814c896b3a956b1e3af7f
Reuben Wong <reuben.wong.ys@gmail.com>
2021-02-26 11:31:39 +0800

    tiling tests passed

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -1,6 +1,7 @@
 from env.maze import Maze
 from env.tiles import TileFactory
 from env.tiles import WhiteTile
+from env.tiles import OrangeTile
 import numpy as np
 
 def test_basic_maze_loading():
@@ -11,10 +12,12 @@ def test_basic_maze_loading():
 
 def test_tile_creation():
     test_map = np.array([[0, 1, 2, 3], [3, 2, 1, 0]])
-    tile_map = np.zeros(test_map.shape)
+    # specify dtype=object or cannot store tiles
+    tile_map = np.zeros(test_map.shape, dtype=object)
     factory = TileFactory()
     for i in range(test_map.shape[0]):
         for j in range(test_map.shape[1]):
             tile_map[i, j] = factory.create_tile(map_value=test_map[i, j], location=(i, j))
 
     assert isinstance(tile_map[0, 0], WhiteTile)
+    assert isinstance(tile_map[1, 0], OrangeTile)
