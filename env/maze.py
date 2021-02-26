d8a17ce579ddf47d146eca792abaf0933c7fa77e
Reuben Wong <reuben.wong.ys@gmail.com>
2021-02-26 12:54:49 +0800

    pass maze construction

--- env/maze.py
+++ env/maze.py
@@ -1,11 +1,21 @@
 import numpy as np
 import os
+from env.tiles import Tile, TileFactory
 
 class Maze(object):
     def __init__(self, maze_path):
         self.default_path = os.path.join(os.getcwd(), 'env/mazes/basic.txt')
         self.maze_path = maze_path if maze_path != None else self.default_path
-        self.maze = np.genfromtxt(self.maze_path, delimiter=',', dtype=np.uint8)
+        self.maze_template = np.genfromtxt(self.maze_path, delimiter=',', dtype=np.uint8)
+        self.shape = self.maze_template.shape
+        self.factory = TileFactory()
+        self._build_maze()
 
-    def is_boundary(self):
+    def is_out_of_bounds(self):
         raise NotImplementedError
+
+    def _build_maze(self):
+        self.maze = np.zeros(self.shape, dtype=object)
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                self.maze[i, j] = self.factory.create_tile(map_value=self.maze_template[i, j], location=(i, j))
--- tests/test_maze.py
+++ tests/test_maze.py
@@ -1,14 +1,15 @@
 from env.maze import Maze
-from env.tiles import TileFactory
+from env.tiles import GreenTile, TileFactory
 from env.tiles import WhiteTile
 from env.tiles import OrangeTile
 import numpy as np
 
 def test_basic_maze_loading():
     maze_path = None
-    test_maze = Maze(maze_path)
+    maze = Maze(maze_path)
+    maze_template = maze.maze_template
 
-    assert test_maze.maze.shape == (6,6)
+    assert maze_template.shape == (6,6)
 
 def test_tile_creation():
     test_map = np.array([[0, 1, 2, 3], [3, 2, 1, 0]])
@@ -21,3 +22,8 @@ def test_tile_creation():
 
     assert isinstance(tile_map[0, 0], WhiteTile)
     assert isinstance(tile_map[1, 0], OrangeTile)
+
+def test_maze_building():
+    maze = Maze(maze_path=None)
+
+    assert isinstance(maze.maze[0, 0], GreenTile)
