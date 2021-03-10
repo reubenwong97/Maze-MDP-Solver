6ccffc937446f3ebaff2b70cc5050bcd026f5832
Reuben Wong <reuben.wong.ys@gmail.com>
2021-03-11 01:11:02 +0800

    added more flags for theta

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--difficulty', default='default')
-parser.add_argument('--gamma', default=0.99)
-parser.add_argument('--theta', default=0.01)
+parser.add_argument('--difficulty', default='default', type=str)
+parser.add_argument('--gamma', default=0.99, type=float)
+parser.add_argument('--vtheta', default=0.01, type=float)
+parser.add_argument('--ptheta', default=0.01, type=float)
 
 args = parser.parse_args()
 
 env = Maze(args.difficulty)
 
 learner = ValueIterationLearner(env=env)
-learner.value_iteration(args.gamma, args.theta)
-learner.plot_value(save_path='visualisations/value_iteration_values_{}.png'.format(env.difficulty))
+learner.value_iteration(args.gamma, args.vtheta)
+learner.plot_value(save_path='visualisations/value_iteration_utilities_{}.png'.format(env.difficulty))
 learner.visualise_policy(save_path='visualisations/value_iteration_policy_{}.png'.format(env.difficulty))
 
 learner = PolicyIterationLearner(env=env)
-learner.policy_iteration(args.gamma, args.theta)
+learner.policy_iteration(args.gamma, args.ptheta)
 learner.plot_value(save_path='visualisations/policy_iteration_utilities_{}.png'.format(env.difficulty))
 learner.visualise_policy(save_path='visualisations/policy_iteration_policy_{}.png'.format(env.difficulty))
